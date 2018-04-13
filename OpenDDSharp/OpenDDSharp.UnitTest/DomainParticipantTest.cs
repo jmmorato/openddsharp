6f625350ab752bd99733321d3b11e2c002c5d0e1
Jose Morato <jmmorato@enxarxats.com>
2018-04-13 14:58:22 +0200

    [feature] Added UnitTest project with a single unit test to confirm that the library is unitestable

---
+++ OpenDDSharp/OpenDDSharp.UnitTest/DomainParticipantTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenDDSharp.DDS;
+using OpenDDSharp.OpenDDS.DCPS;
+
+namespace OpenDDSharp.UnitTest
+{
+    [TestClass]
+    public class DomainParticipantTest
+    {
+        [TestMethod]
+        [TestCategory("DomainParticipant")]
+        public void TestDomainId()
+        {
+            DomainParticipantFactory dpf = ParticipantService.Instance.GetDomainParticipantFactory();
+            DomainParticipant participant = dpf.CreateParticipant(42);
+
+            Assert.IsNotNull(participant);
+            Assert.AreEqual(42, participant.DomainId);
+
+            dpf.DeleteParticipant(participant);
+            ParticipantService.Instance.Shutdown();
+        }
+    }
+}
