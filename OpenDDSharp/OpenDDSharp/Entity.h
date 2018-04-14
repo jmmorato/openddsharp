4753a300f37cb19dcbd516db4c7ac3ec10cf55b1
Jose Morato <jmmorato@enxarxats.com>
2018-04-14 11:21:48 +0200

    [ignore] Fixed paragraph documentation mark-up

--- OpenDDSharp/OpenDDSharp/Entity.h
+++ OpenDDSharp/OpenDDSharp/Entity.h
@@ -45,9 +45,9 @@ namespace OpenDDSharp {
 			/// value has changed since the last time the application read the status.
 			/// </summary>
 			/// <remarks>
-			/// <p>When the <see cref="Entity" /> is first created or if the entity is not enabled, all communication statuses are in the “untriggered”.</p>
-			/// <p>The statuses returned by the StatusChanges property refers to the status that are triggered on the <see cref="Entity" /> itself
-			/// and does not include statuses that apply to contained entities.</p>
+			/// <para>When the <see cref="Entity" /> is first created or if the entity is not enabled, all communication statuses are in the “untriggered”.</para>
+			/// <para>The statuses returned by the StatusChanges property refers to the status that are triggered on the <see cref="Entity" /> itself
+			/// and does not include statuses that apply to contained entities.</para>
 			/// </remarks>
 			property OpenDDSharp::DDS::StatusMask StatusChanges {
 				OpenDDSharp::DDS::StatusMask get();
@@ -69,11 +69,11 @@ namespace OpenDDSharp {
 			/// The enable operation is idempotent. Calling enable on an already enabled <see cref="Entity" /> returns <see cref="ReturnCode::Ok" /> and has no effect.
 			/// </summary>
 			/// <remarks>
-			/// <p>Entity objects can be created either enabled or disabled. This is controlled by the value of
+			/// <para>Entity objects can be created either enabled or disabled. This is controlled by the value of
 			/// the <see cref="EntityFactoryQosPolicy" /> on the corresponding QoS for the <see cref="Entity" />. 
 			/// The default setting of <see cref="EntityFactoryQosPolicy" /> is such that, by default, it is not necessary to explicitly call enable on newly
-			/// created entities</p>
-			/// <p>If an <see cref="Entity" /> has not yet been enabled, the following kinds of operations may be invoked on it:
+			/// created entities</para>
+			/// <para>If an <see cref="Entity" /> has not yet been enabled, the following kinds of operations may be invoked on it:
 			/// <list type="bullet">
 			///		<item><description>Operations to set or get an Entity’s QoS policies (including default QoS policies) and listener</description></item>
 			///		<item><description>Access to the <see cref="Entity::StatusCondition" /> property</description></item>
@@ -82,13 +82,13 @@ namespace OpenDDSharp {
 			///		<item><description>'lookup' operations</description></item>
 			/// </list>
 			/// Other operations may explicitly state that they may be called on disabled entities; those that do not will return the error
-			/// <see cref="ReturnCode::NotEnabled" />.
-			/// <p>It is legal to delete an <see cref="Entity" /> that has not been enabled by calling the proper operation on its factory.<p/>
-			/// <p>Entities created from a factory that is disabled, are created disabled regardless of the setting of the <see cref="EntityFactoryQosPolicy" />.</p>
-			/// <p>Calling enable on an <see cref="Entity" /> whose factory is not enabled will fail and return <see cref="ReturnCode::PreconditionNotMet" />.</p>
-			/// <p>If the <see cref="EntityFactoryQosPolicy" /> has <see cref="EntityFactoryQosPolicy::AutoenableCreatedEntities" /> set to <see langword="true"/>, 
-			/// the enable operation on the factory will automatically enable all entities created from the factory.</p>
-			/// <p>The listeners associated with an <see cref="Entity" /> are not called until the entity is enabled.</p>
+			/// <see cref="ReturnCode::NotEnabled" /></para>.
+			/// <para>It is legal to delete an <see cref="Entity" /> that has not been enabled by calling the proper operation on its factory.<para/>
+			/// <para>Entities created from a factory that is disabled, are created disabled regardless of the setting of the <see cref="EntityFactoryQosPolicy" />.</para>
+			/// <para>Calling enable on an <see cref="Entity" /> whose factory is not enabled will fail and return <see cref="ReturnCode::PreconditionNotMet" />.</para>
+			/// <para>If the <see cref="EntityFactoryQosPolicy" /> has <see cref="EntityFactoryQosPolicy::AutoenableCreatedEntities" /> set to <see langword="true"/>, 
+			/// the enable operation on the factory will automatically enable all entities created from the factory.</para>
+			/// <para>The listeners associated with an <see cref="Entity" /> are not called until the entity is enabled.</para>
 			/// </remarks>
 			OpenDDSharp::DDS::ReturnCode Enable();
 		
--- OpenDDSharp/OpenDDSharp/MultiTopic.h
+++ OpenDDSharp/OpenDDSharp/MultiTopic.h
@@ -22,27 +22,27 @@ namespace OpenDDSharp {
 		/// (aggregation/projection) according to a subscription expression with the expression parameters.
 		/// </summary>
 		/// <remarks>
-		/// <p>The subscription expression is a string that identifies the selection and re-arrangement of data from the associated
+		/// <para>The subscription expression is a string that identifies the selection and re-arrangement of data from the associated
 		/// topics. It is similar to an SQL clause where the SELECT part provides the fields to be kept, the FROM part provides the
 		///	names of the topics that are searched for those fields, and the WHERE clause gives the content filter.The topics
 		///	combined may have different types but they are restricted in that the type of the fields used for the NATURAL JOIN
-		///	operation must be the same.</p>
-		/// <p>The expression parameters are a collection of strings that give values to the ‘parameters’ ("%n" tokens) in
+		///	operation must be the same.</para>
+		/// <para>The expression parameters are a collection of strings that give values to the ‘parameters’ ("%n" tokens) in
 		/// the subscription expression. The number of supplied parameters must fit with the requested values in the
-		///	subscription expression (the number of %n tokens).</p>
-		/// <p><see cref="DataReader" /> entities associated with a MultiTopic are alerted of data modifications by the usual listener or condition
-		/// mechanisms whenever modifications occur to the data associated with any of the topics relevant to the MultiTopic.</p>
-		/// <p><see cref="DataReader" /> entities associated with a MultiTopic access instances that are “constructed” at the <see cref="DataReader" /> side from
+		///	subscription expression (the number of %n tokens).</para>
+		/// <para><see cref="DataReader" /> entities associated with a MultiTopic are alerted of data modifications by the usual listener or condition
+		/// mechanisms whenever modifications occur to the data associated with any of the topics relevant to the MultiTopic.</para>
+		/// <para><see cref="DataReader" /> entities associated with a MultiTopic access instances that are “constructed” at the <see cref="DataReader" /> side from
 		/// the instances written by multiple <see cref="DataWriter" /> entities.The MultiTopic access instance will begin to exist as soon as all
-		///	the constituting Topic instances are in existence.</p>
-		/// <p>The view_state and instance_state is computed from the corresponding states of the constituting instances:</p>
+		///	the constituting Topic instances are in existence.</para>
+		/// <para>The view_state and instance_state is computed from the corresponding states of the constituting instances:
 		/// <list type="bullet">
 		///		<item><description>The view state of the MultiTopic instance is <see cref="ViewStateKind::NewViewState" /> if at least one of the constituting instances has
 		/// <see cref="ViewStateKind::NewViewState" />, otherwise it will be  <see cref="ViewStateKind::NotNewViewState" />.</description></item>
 		///		<item><description>The instance state of the MultiTopic instance is <see cref="InstanceStateKind::AliveInstanceState" /> if the instance state of all the
 		/// constituting Topic instances is <see cref="InstanceStateKind::AliveInstanceState" />. It is <see cref="InstanceStateKind::NotAliveDisposedInstanceState" /> if at least one of the constituting <see cref="Topic" />
 		///	instances is <see cref="InstanceStateKind::NotAliveDisposedInstanceState" />. Otherwise it is <see cref="InstanceStateKind::NotAliveNoWritersInstanceState" />.</description></item>
-		///	</list>
+		///	</list></para>
 		/// </remarks>
 		public ref class MultiTopic : public OpenDDSharp::DDS::TopicDescription {
 
