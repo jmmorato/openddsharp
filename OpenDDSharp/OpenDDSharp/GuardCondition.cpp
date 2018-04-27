8638c6aa2912726a1949d81c96956d89ce9c7ead
Jose Morato <jmmorato@enxarxats.com>
2018-04-27 18:21:09 +0200

    [breaking] Replaced the GetTriggerValue and SetTriggerValue public methods with the read and write property TriggerValue

--- OpenDDSharp/OpenDDSharp/GuardCondition.cpp
+++ OpenDDSharp/OpenDDSharp/GuardCondition.cpp
@@ -8,13 +8,12 @@ OpenDDSharp::DDS::GuardCondition::GuardCondition() : GuardCondition(new ::DDS::G
 
 }
 
-
-System::Boolean OpenDDSharp::DDS::GuardCondition::GetTriggerValue() {
+System::Boolean OpenDDSharp::DDS::GuardCondition::TriggerValue::get() {
 	return impl_entity->get_trigger_value();
 }
 
-OpenDDSharp::DDS::ReturnCode OpenDDSharp::DDS::GuardCondition::SetTriggerValue(System::Boolean value) {
-	return (OpenDDSharp::DDS::ReturnCode)impl_entity->set_trigger_value(value);
+void OpenDDSharp::DDS::GuardCondition::TriggerValue::set(System::Boolean value) {
+	impl_entity->set_trigger_value(value);
 }
 
 OpenDDSharp::DDS::ReturnCode OpenDDSharp::DDS::GuardCondition::AttachToWaitSet(OpenDDSharp::DDS::WaitSet^ ws) {
--- OpenDDSharp/OpenDDSharp/GuardCondition.h
+++ OpenDDSharp/OpenDDSharp/GuardCondition.h
@@ -10,21 +10,57 @@
 
 namespace OpenDDSharp {
 	namespace DDS {
+
+		/// <summary>
+		/// A GuardCondition object is a specific <see cref="Condition" /> whose <see cref="Condition::TriggerValue" /> is completely under the control of the application.
+		/// </summary>
+		/// <remarks>
+		/// <para>GuardCondition has no factory. When first created the <see cref="Condition::TriggerValue" /> is set to <see langword="false"/>.</para>
+		/// <para>The purpose of the GuardCondition is to provide the means for the application to manually wakeup a <see cref="WaitSet" />. This is
+		/// accomplished by attaching the GuardCondition to the <see cref="WaitSet" /> and then setting the <see cref="Condition::TriggerValue" /> by means of the
+		/// <see cref="TriggerValue" /> set operation.</para>
+		/// </remarks>
 		public ref class GuardCondition : public OpenDDSharp::DDS::Condition {
 
 		internal:
 			::DDS::GuardCondition_ptr impl_entity;
 
+		public:
+			/// <summary>
+			/// Gets/Sets the trigger value of the <see cref="GuardCondition" />
+			/// </summary>
+			property System::Boolean TriggerValue  {
+				System::Boolean get();
+				void set(System::Boolean value);
+			}
+
 		internal:
 			GuardCondition(::DDS::GuardCondition_ptr native);
+
 		public:
+			/// <summary>
+			/// Creates a new instance of the <see cref="GuardCondition" />
+			/// </summary>
 			GuardCondition();
 
 		public:
-			System::Boolean GetTriggerValue();
-			OpenDDSharp::DDS::ReturnCode SetTriggerValue(System::Boolean value);
+			/// <summary>
+			/// Attach the <see cref="GuardCondition" /> to a <see cref="WaitSet" />
+			/// </summary>
+			/// <param name="ws">The <see cref="WaitSet" /> to be attached.</param>
+			/// <returns>The <see cref="ReturnCode" /> that indicates the operation result.</returns>
 			OpenDDSharp::DDS::ReturnCode AttachToWaitSet(OpenDDSharp::DDS::WaitSet^ ws);
+
+			/// <summary>
+			/// Detach the <see cref="GuardCondition" /> from a <see cref="WaitSet" />
+			/// </summary>
+			/// <param name="ws">The <see cref="WaitSet" /> to be detached from.</param>
+			/// <returns>The <see cref="ReturnCode" /> that indicates the operation result.</returns>
 			OpenDDSharp::DDS::ReturnCode DetachFromWaitSet(OpenDDSharp::DDS::WaitSet^ ws);
+
+			/// <summary>
+			/// Send a signal to all the attached <see cref="WaitSet" />s. 
+			/// </summary>
 			void SignalAll();
 
 		};
