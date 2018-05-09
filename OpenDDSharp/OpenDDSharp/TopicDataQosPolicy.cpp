c62b9853417fd7ac0f6754018eb0aafd2803765e
Jose Morato <jmmorato@enxarxats.com>
2018-05-09 17:36:34 +0200

    [ignore] Added file header as recommended by GPLv3

--- OpenDDSharp/OpenDDSharp/TopicDataQosPolicy.cpp
+++ OpenDDSharp/OpenDDSharp/TopicDataQosPolicy.cpp
@@ -1,3 +1,22 @@
+/*********************************************************************
+This file is part of OpenDDSharp.
+
+OpenDDSharp is a .NET wrapper for OpenDDS
+Copyright (C) 2018 Jose Morato
+
+OpenDDSharp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenDDSharp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
 #include "TopicDataQosPolicy.h"
 
 OpenDDSharp::DDS::TopicDataQosPolicy::TopicDataQosPolicy() {
