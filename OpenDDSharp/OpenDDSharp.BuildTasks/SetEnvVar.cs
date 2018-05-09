4b1e5ad42cc0ae55fb48fd6349f8b66e70999833
Jose Morato <jmmorato@enxarxats.com>
2018-05-09 17:40:30 +0200

    [ignore] Added file header as recommended by GPLv3 license

--- OpenDDSharp/OpenDDSharp.BuildTasks/SetEnvVar.cs
+++ OpenDDSharp/OpenDDSharp.BuildTasks/SetEnvVar.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*********************************************************************
+This file is part of OpenDDSharp.
+
+OpenDDSharp is a .NET wrapper for OpenDDS.
+Copyright (C) 2018 Jose Morato
+
+OpenDDSharp is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenDDSharp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+using System;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
 
