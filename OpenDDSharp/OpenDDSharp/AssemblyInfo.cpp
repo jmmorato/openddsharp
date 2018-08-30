a29475224dfd7611f1a439e9edb829d9c9dba659
Jose Morato <jmmorato@enxarxats.com>
2018-08-30 21:11:10 +0200

    [feature] Adde assembly info

---
+++ OpenDDSharp/OpenDDSharp/AssemblyInfo.cpp
@@ -0,0 +1,32 @@
+using namespace System;
+using namespace System::Reflection;
+using namespace System::Runtime::InteropServices;
+
+#include "AssemblyInfo.h"
+
+//
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+[assembly:AssemblyTitleAttribute(Assembly_Title)];
+[assembly:AssemblyDescriptionAttribute(Assembly_Description)];
+[assembly:AssemblyConfigurationAttribute(Assembly_Configuration)];
+[assembly:AssemblyCompanyAttribute(Assembly_Company)];
+[assembly:AssemblyProductAttribute(Assembly_Product)];
+[assembly:AssemblyCopyrightAttribute(Assembly_Copyright)];
+
+//
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+
+[assembly:AssemblyVersionAttribute(Assembly_Version)];
+
+[assembly:ComVisible(false)];
+
+[assembly:CLSCompliantAttribute(false)];
---
+++ OpenDDSharp/OpenDDSharp/AssemblyInfo.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define Internal_Name "OpenDDSharp.dll"
+#define Original_Filename "OpenDDSharp.dll"
+#define Product_Name "OpenDDSharp"
+#define Assembly_Title L"OpenDDSharp"
+#define Assembly_Configuration L""
+#define Assembly_Company L"OpenDDSharp"
+#define Assembly_Product L"OpenDDSharp"
+#define Assembly_Copyright L"Copyright © OpenDDSharp 2018. All rights reserved"
+#define Assembly_Description L"OpenDDSharp"
+#define File_Version 0,2,1,0
+#define File_Version_Str "0.2.1.0"
+#define Assembly_Version L"0.2.1.0"
