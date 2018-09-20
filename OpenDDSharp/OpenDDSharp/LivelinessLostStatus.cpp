99176d1c62a5f3cbb5bc9e461088c17da26eda6c
Jose Morato <jmmorato@enxarxats.com>
2018-09-20 11:10:41 +0200

    [ignore] Adapted license boilerplate to LGPL. Related issue #9

--- OpenDDSharp/OpenDDSharp/LivelinessLostStatus.cpp
+++ OpenDDSharp/OpenDDSharp/LivelinessLostStatus.cpp
@@ -5,16 +5,16 @@ OpenDDSharp is a .NET wrapper for OpenDDS
 Copyright (C) 2018 Jose Morato
 
 OpenDDSharp is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
+it under the terms of the GNU Lesser General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
 OpenDDSharp is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+GNU Lesser General Public License for more details.
 
-You should have received a copy of the GNU General Public License
+You should have received a copy of the GNU Lesser General Public License
 along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
 **********************************************************************/
 #include "LivelinessLostStatus.h"
