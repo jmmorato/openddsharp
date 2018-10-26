5044ff4b9f35e3ee3e8881fc56ebc51c81f0c4b4
Jose Morato <jmmorato@enxarxats.com>
2018-10-26 18:30:04 +0200

    [fix] Fixed DeleteContainedEntities condition for internal call to ClearContainedEntities

--- OpenDDSharp/OpenDDSharp/DataReader.cpp
+++ OpenDDSharp/OpenDDSharp/DataReader.cpp
@@ -104,7 +104,7 @@ OpenDDSharp::DDS::ReturnCode OpenDDSharp::DDS::DataReader::DeleteReadCondition(O
 OpenDDSharp::DDS::ReturnCode OpenDDSharp::DDS::DataReader::DeleteContainedEntities() {
     ::DDS::ReturnCode_t ret = impl_entity->delete_contained_entities();
 
-    if (ret != ::DDS::RETCODE_OK) {
+    if (ret == ::DDS::RETCODE_OK) {
         ClearContainedEntities();
     }
 
@@ -142,10 +142,10 @@ OpenDDSharp::DDS::ReturnCode OpenDDSharp::DDS::DataReader::SetListener(OpenDDSha
 	_listener = listener;
 
 	if (_listener != nullptr) {
-		return (OpenDDSharp::DDS::ReturnCode)impl_entity->set_listener(listener->impl_entity, (System::UInt32)mask);
+		return (OpenDDSharp::DDS::ReturnCode)impl_entity->set_listener(listener->impl_entity, (::DDS::StatusMask)mask);
 	}
 	else {
-		return (OpenDDSharp::DDS::ReturnCode)impl_entity->set_listener(NULL, (System::UInt32)mask);
+		return (OpenDDSharp::DDS::ReturnCode)impl_entity->set_listener(NULL, (::DDS::StatusMask)mask);
 	}
 }
 
