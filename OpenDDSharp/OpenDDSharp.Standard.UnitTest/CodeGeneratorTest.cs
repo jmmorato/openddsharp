9a65907aff399304194377c7657160dab7598c02
Jose Morato <jmmorato@enxarxats.com>
2019-11-13 18:01:37 +0100

    [feature] New unit test project for the standard libraries

---
+++ OpenDDSharp/OpenDDSharp.Standard.UnitTest/CodeGeneratorTest.cs
@@ -0,0 +1,131 @@
+/*********************************************************************
+This file is part of OpenDDSharp.
+
+OpenDDSharp is a .NET wrapper for OpenDDS
+Copyright (C) 2018 Jose Morato
+
+OpenDDSharp is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenDDSharp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenDDSharp.DDS;
+using OpenDDSharp.Standard.UnitTest.Helpers;
+using Test;
+
+namespace OpenDDSharp.Standard.UnitTest
+{
+    [TestClass]
+    public class CodeGeneratorTest
+    {
+        #region Constants
+        private const string TEST_CATEGORY = "Standard.CodeGenerator";
+        #endregion
+
+        #region Fields
+        private DomainParticipant _participant;
+        private Publisher _publisher;
+        private Subscriber _subscriber;
+        private Topic _topic;
+        private DataReader _dr;
+        private TestStructDataReader _dataReader;
+        private DataWriter _dw;
+        private TestStructDataWriter _dataWriter;
+        #endregion
+
+        #region Initialization/Cleanup
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _participant = AssemblyInitializer.Factory.CreateParticipant(AssemblyInitializer.RTPS_DOMAIN);
+            Assert.IsNotNull(_participant);
+            
+            _publisher = _participant.CreatePublisher();
+            Assert.IsNotNull(_publisher);
+
+            _subscriber = _participant.CreateSubscriber();
+            Assert.IsNotNull(_publisher);
+
+            TestStructTypeSupport typeSupport = new TestStructTypeSupport();
+            string typeName = typeSupport.GetTypeName();
+            ReturnCode ret = typeSupport.RegisterType(_participant, typeName);
+            Assert.AreEqual(ReturnCode.Ok, ret);
+
+            _topic = _participant.CreateTopic("TestTopic", typeName);
+            Assert.IsNotNull(_topic);
+
+            _dr = _subscriber.CreateDataReader(_topic);
+            Assert.IsNotNull(_dr);            
+            _dataReader = new TestStructDataReader(_dr);
+
+            _dw = _publisher.CreateDataWriter(_topic);
+            Assert.IsNotNull(_dw);            
+            _dataWriter = new TestStructDataWriter(_dw);
+
+            Assert.IsTrue(_dataWriter.WaitForSubscriptions(1, 1000));
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            //if (_participant != null)
+            //{
+            //    ReturnCode result = _participant.DeleteContainedEntities();
+            //    Assert.AreEqual(ReturnCode.Ok, result);
+            //}
+
+            //if (AssemblyInitializer.Factory != null)
+            //{
+            //    ReturnCode result = AssemblyInitializer.Factory.DeleteParticipant(_participant);
+            //    Assert.AreEqual(ReturnCode.Ok, result);
+            //}
+        }
+        #endregion
+
+        #region Test Methods
+        [TestMethod, TestCategory(TEST_CATEGORY)]
+        public void TestGeneratedIntegers()
+        {
+            TestStruct data = new TestStruct
+            {
+                ShortField = -1,
+                LongField = -2,
+                LongLongField = -3,
+                UnsignedShortField = 1,
+                UnsignedLongField = 2,
+                UnsignedLongLongField = 3,
+            };
+            _dataWriter.Write(data);
+
+            System.Threading.Thread.Sleep(500);
+
+            TestStruct received = new TestStruct();
+            var ret = _dataReader.ReadNextSample(received);
+            
+            Assert.AreEqual(ReturnCode.Ok, ret);
+            Assert.AreEqual(data.ShortField, received.ShortField);
+            Assert.AreEqual(data.LongField, received.LongField);
+            Assert.AreEqual(data.LongLongField, received.LongLongField);
+            Assert.AreEqual(data.UnsignedShortField, received.UnsignedShortField);
+            Assert.AreEqual(data.UnsignedLongField, received.UnsignedLongField);
+            Assert.AreEqual(data.UnsignedLongLongField, received.UnsignedLongLongField);
+
+            Assert.AreEqual(typeof(short), data.ShortField.GetType());
+            Assert.AreEqual(typeof(int), data.LongField.GetType());
+            Assert.AreEqual(typeof(long), data.LongLongField.GetType());
+            Assert.AreEqual(typeof(ushort), data.UnsignedShortField.GetType());
+            Assert.AreEqual(typeof(uint), data.UnsignedLongField.GetType());
+            Assert.AreEqual(typeof(ulong), data.UnsignedLongLongField.GetType());
+        }
+        #endregion
+    }
+}
---
+++ OpenDDSharp/OpenDDSharp.Standard.UnitTest/Helpers/TestHelper.cs
@@ -0,0 +1,73 @@
+﻿/*********************************************************************
+This file is part of OpenDDSharp.
+
+OpenDDSharp is a .NET wrapper for OpenDDS
+Copyright (C) 2018 Jose Morato
+
+OpenDDSharp is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenDDSharp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+using System.Collections.Generic;
+using System.Threading;
+using OpenDDSharp.DDS;
+
+namespace OpenDDSharp.Standard.UnitTest.Helpers
+{
+    public static class TestHelper
+    {
+        #region Extensions
+        public static bool WaitForSubscriptions(this DataWriter writer, int subscriptionsCount, int milliseconds)
+        {
+            //PublicationMatchedStatus status = new PublicationMatchedStatus();
+            //writer.GetPublicationMatchedStatus(ref status);
+            //int count = milliseconds / 100;
+            //while (status.CurrentCount != subscriptionsCount && count > 0)
+            //{
+            //    System.Threading.Thread.Sleep(100);
+            //    writer.GetPublicationMatchedStatus(ref status);
+            //    count--;
+            //}
+
+            //if (count == 0 && status.CurrentCount != subscriptionsCount)
+            //{
+            //    return false;
+            //}
+            Thread.Sleep(500);
+
+            return true;
+        }
+
+        public static bool WaitForPublications(this DataReader reader, int publicationsCount, int milliseconds)
+        {
+            //List<InstanceHandle> handles = new List<InstanceHandle>();
+            //reader.GetMatchedPublications(handles);
+            //int count = milliseconds / 100;
+            //while (handles.Count != publicationsCount && count > 0)
+            //{
+            //    System.Threading.Thread.Sleep(100);
+            //    reader.GetMatchedPublications(handles);
+            //    count--;
+            //}
+
+            //if (count == 0 && handles.Count != publicationsCount)
+            //{
+            //    return false;
+            //}
+
+            Thread.Sleep(500);
+
+            return true;
+        }
+        #endregion
+    }
+}
