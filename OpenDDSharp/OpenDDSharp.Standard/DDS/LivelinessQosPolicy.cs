85cf29e1f713d7611ff2a311c294697c061ca725
Jose Morato <jmmorato@enxarxats.com>
2020-03-03 20:06:10 +0100

    [feature] Implemented LivelinessQosPolicy structure

---
+++ OpenDDSharp/OpenDDSharp.Standard/DDS/LivelinessQosPolicy.cs
@@ -0,0 +1,119 @@
+﻿/*********************************************************************
+This file is part of OpenDDSharp.
+
+OpenDDSharp is a .NET wrapper for OpenDDS
+Copyright (C) 2018 Jose Morato
+
+OpenDDSharp is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenDDSharp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+using System;
+using System.Runtime.InteropServices;
+
+namespace OpenDDSharp.DDS
+{
+    /// <summary>
+    /// This policy controls the mechanism and parameters used by DDS to ensure that particular entities on the network are still "alive".
+    /// </summary>
+    /// <remarks>
+    /// The value offered is considered compatible with the value requested if and only if the following conditions are met:
+    /// <list type="bullet">
+    ///     <item><description>The inequality "offered Kind &gt;= requested Kind" evaluates to 'true'. For the purposes of this inequality, the values of Liveliness kind are considered ordered such that: Automatic &lt; ManualByParticipant &lt; ManualByTopic.</description></item>
+    ///     <item><description>The inequality "offered LeaseDuration &lt;= requested LeaseDuration" evaluates to 'true'.</description></item>
+    /// </list>
+    /// </remarks>
+    [StructLayout(LayoutKind.Sequential)]
+    public struct LivelinessQosPolicy : IEquatable<LivelinessQosPolicy>
+    {
+        #region Properties
+        /// <summary>
+        /// Gets or sets the liveliness kind applied to the <see cref="Entity" />.
+        /// </summary>
+        public LivelinessQosPolicyKind Kind { get; set; }
+
+        /// <summary>
+        /// Gets or sets the liveliness lease duration.
+        /// </summary>
+        public Duration LeaseDuration { get; set; }
+        #endregion
+
+        #region IEquatable<LivelinessQosPolicy> Members
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns><see langword="true" /> if the current object is equal to the other parameter; otherwise, <see langword="false" />.</returns>
+        public bool Equals(LivelinessQosPolicy other)
+        {
+            return Kind == other.Kind &&
+                   LeaseDuration == other.LeaseDuration;
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" />.</returns>
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            if (GetType() != obj.GetType())
+            {
+                return false;
+            }
+
+            return Equals((LivelinessQosPolicy)obj);
+        }
+
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            var hashCode = 1464949012;
+            hashCode = (hashCode * -1521134295) + Kind.GetHashCode();
+            hashCode = (hashCode * -1521134295) + LeaseDuration.GetHashCode();
+            return hashCode;
+        }
+        #endregion
+
+        #region Operators
+        /// <summary>
+        /// Equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="true" /> if the left object is equal to the right object; otherwise, <see langword="false" />.</returns>
+        public static bool operator ==(LivelinessQosPolicy left, LivelinessQosPolicy right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Not equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="false" /> if the left object is equal to the right object; otherwise, <see langword="true" />.</returns>
+        public static bool operator !=(LivelinessQosPolicy left, LivelinessQosPolicy right)
+        {
+            return !left.Equals(right);
+        }
+        #endregion
+    }
+}
