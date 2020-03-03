6178f24c1ef51403f51361b34876d375bca0a8fe
Jose Morato <jmmorato@enxarxats.com>
2020-03-03 18:28:18 +0100

    [feature] Implemented Duration structure

--- OpenDDSharp/OpenDDSharp.Standard/DDS/Duration.cs
+++ OpenDDSharp/OpenDDSharp.Standard/DDS/Duration.cs
@@ -63,16 +63,30 @@ namespace OpenDDSharp.DDS
         #endregion
 
         #region IEquatable<Duration> Members
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns><see langword="true" /> if the current object is equal to the other parameter; otherwise, <see langword="false" />.</returns>
         public bool Equals(Duration other)
         {
             return Seconds == other.Seconds && NanoSeconds == other.NanoSeconds;
         }
 
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" />.</returns>
         public override bool Equals(object obj)
         {
             return (obj is Duration other) && Equals(other);
         }
 
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
         public override int GetHashCode()
         {
             var hashCode = -1725149974;
@@ -83,11 +97,23 @@ namespace OpenDDSharp.DDS
         #endregion
 
         #region Operators
+        /// <summary>
+        /// Equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="true" /> if the left object is equal to the right object; otherwise, <see langword="false" />.</returns>
         public static bool operator ==(Duration left, Duration right)
         {
             return left.Equals(right);
         }
 
+        /// <summary>
+        /// Not equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="false" /> if the left object is equal to the right object; otherwise, <see langword="true" />.</returns>
         public static bool operator !=(Duration left, Duration right)
         {
             return !(left == right);
