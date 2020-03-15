70db40740624a16712b07b4cf7255ea5ca8f5679
Jose Morato <jmmorato@enxarxats.com>
2020-03-15 10:27:55 +0100

    [feature] Implemented UserDataQosPolicy

--- OpenDDSharp/OpenDDSharp.Standard/DDS/UserDataQosPolicy.cs
+++ OpenDDSharp/OpenDDSharp.Standard/DDS/UserDataQosPolicy.cs
@@ -18,17 +18,168 @@ You should have received a copy of the GNU Lesser General Public License
 along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
 **********************************************************************/
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Runtime.InteropServices;
+using OpenDDSharp.Helpers;
 
 namespace OpenDDSharp.DDS
 {
-    public class UserDataQosPolicy
+    /// <summary>
+    /// The purpose of this QoS is to allow the application to attach additional information to the created <see cref="Entity" /> objects such that when
+    /// a remote application discovers their existence it can access that information and use it for its own purposes.
+    /// </summary>
+    public sealed class UserDataQosPolicy : IEquatable<UserDataQosPolicy>
     {
+        #region Fields
+        private readonly List<IntPtr> toRelease = new List<IntPtr>();
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the bytes assigned to the <see cref="UserDataQosPolicy" />.
+        /// </summary>
+        public IList<byte> Value { get; set; }
+        #endregion
+
+        #region Constructors
+        internal UserDataQosPolicy()
+        {
+            Value = new List<byte>();
+        }
+        #endregion
+
+        #region Methods
+        internal UserDataQosPolicyWrapper ToNative()
+        {
+            IntPtr ptr = IntPtr.Zero;
+
+            if (Value != null)
+            {
+                Value.SequenceToPtr(ref ptr);
+                toRelease.Add(ptr);
+            }
+
+            return new UserDataQosPolicyWrapper
+            {
+                Value = ptr,
+            };
+        }
+
+        internal void FromNative(UserDataQosPolicyWrapper wrapper)
+        {
+            IList<byte> list = new List<byte>();
+
+            if (wrapper.Value != IntPtr.Zero)
+            {
+                wrapper.Value.PtrToSequence(ref list);
+            }
+
+            Value = list;
+        }
+
+        internal void Release()
+        {
+            if (toRelease == null)
+            {
+                return;
+            }
+
+            foreach (IntPtr ptr in toRelease)
+            {
+                Marshal.FreeHGlobal(ptr);
+            }
+
+            toRelease.Clear();
+        }
+        #endregion
+
+        #region IEquatable<UserDataQosPolicy> Members
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns><see langword="true" /> if the current object is equal to the other parameter; otherwise, <see langword="false" />.</returns>
+        public bool Equals(UserDataQosPolicy other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+
+            return Value.SequenceEqual(other.Value);
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" />.</returns>
+        public override bool Equals(object obj)
+        {
+            return (obj is UserDataQosPolicy other) && Equals(other);
+        }
+
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            return -1937169414 + EqualityComparer<IEnumerable<byte>>.Default.GetHashCode(Value);
+        }
+        #endregion
+
+        #region Operators
+        /// <summary>
+        /// Equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="true" /> if the left object is equal to the right object; otherwise, <see langword="false" />.</returns>
+        public static bool operator ==(UserDataQosPolicy left, UserDataQosPolicy right)
+        {
+            if (left == null && right == null)
+            {
+                return true;
+            }
+
+            if (left == null || right == null)
+            {
+                return false;
+            }
+
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Not equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="false" /> if the left object is equal to the right object; otherwise, <see langword="true" />.</returns>
+        public static bool operator !=(UserDataQosPolicy left, UserDataQosPolicy right)
+        {
+            if (left == null && right == null)
+            {
+                return false;
+            }
+
+            if (left == null || right == null)
+            {
+                return true;
+            }
+
+            return !left.Equals(right);
+        }
+        #endregion
     }
 
     [StructLayout(LayoutKind.Sequential)]
     internal struct UserDataQosPolicyWrapper
     {
-        IntPtr value;
+        #region Fields
+        public IntPtr Value;
+        #endregion
     }
 }
