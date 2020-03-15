72c3acb49702c8dee1cdcf8fda1036d1373e9945
Jose Morato <jmmorato@enxarxats.com>
2020-03-15 10:34:13 +0100

    [feature] Added new marshal support methods

--- OpenDDSharp/OpenDDSharp.Standard/Helpers/MarshalHelper.cs
+++ OpenDDSharp/OpenDDSharp.Standard/Helpers/MarshalHelper.cs
@@ -74,6 +74,158 @@ namespace OpenDDSharp.Helpers
             }
         }
 
+        public static void PtrToSequence<T>(this IntPtr ptr, ref IList<T> sequence, int capacity = 0)
+        {
+            // Ensure a not null empty list to populate
+            if (sequence == null)
+            {
+                if (capacity > 0)
+                {
+                    sequence = new List<T>(capacity);
+                }
+                else
+                {
+                    sequence = new List<T>();
+                }
+            }
+            else
+            {
+                sequence.Clear();
+            }
+
+            if (ptr == IntPtr.Zero)
+            {
+                return;
+            }
+
+            // Start by reading the size of the array
+            int length = Marshal.ReadInt32(ptr);
+
+            // For efficiency, only compute the element size once
+            int elSiz = Marshal.SizeOf<T>();
+
+            // Populate the list
+            for (int i = 0; i < length; i++)
+            {
+                sequence.Add(Marshal.PtrToStructure<T>(ptr + sizeof(int) + (elSiz * i)));
+            }
+        }
+
+        public static void SequenceToPtr<T>(this IList<T> sequence, ref IntPtr ptr)
+        {
+            if (sequence == null || sequence.Count == 0)
+            {
+                // No structures in the list. Write 0 and return
+                ptr = Marshal.AllocHGlobal(sizeof(int));
+                Marshal.WriteInt32(ptr, 0);
+                return;
+            }
+
+            int elSiz = Marshal.SizeOf<T>();
+            // Get the total size of unmanaged memory that is needed (length + elements)
+            int size = sizeof(int) + (elSiz * sequence.Count);
+            // Allocate unmanaged space.
+            ptr = Marshal.AllocHGlobal(size);
+            // Write the "Length" field first
+            Marshal.WriteInt32(ptr, sequence.Count);
+            // Write the list data
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                // Newly-allocated space has no existing object, so the last param is false 
+                Marshal.StructureToPtr(sequence[i], ptr + sizeof(int) + (elSiz * i), false);
+            }
+        }
+
+        public static void PtrToStringSequence(this IntPtr ptr, ref IList<string> sequence, bool isUnicode, int capacity = 0)
+        {
+            // Ensure a not null empty list to populate
+            if (sequence == null)
+            {
+                if (capacity > 0)
+                {
+                    sequence = new List<string>(capacity);
+                }
+                else
+                {
+                    sequence = new List<string>();
+                }
+            }
+            else
+            {
+                sequence.Clear();
+            }
+
+            if (ptr == IntPtr.Zero)
+            {
+                return;
+            }
+
+            // Start by reading the size of the array
+            int length = Marshal.ReadInt32(ptr);
+
+            // Populate the array
+            for (int i = 0; i < length; i++)
+            {
+                // Get the unmanaged pointer
+                IntPtr pointer = Marshal.PtrToStructure<IntPtr>(ptr + sizeof(int) + (IntPtr.Size * i));
+
+                // Convert the pointer in a string
+                if (isUnicode)
+                {
+                    sequence.Add(Marshal.PtrToStringUni(pointer));
+                }
+                else
+                {
+                    sequence.Add(Marshal.PtrToStringAnsi(pointer));
+                }
+            }
+        }
+
+        public static List<IntPtr> StringSequenceToPtr(this IList<string> sequence, ref IntPtr ptr, bool isUnicode)
+        {
+            List<IntPtr> toRelease = new List<IntPtr>();
+
+            if (sequence == null || sequence.Count == 0)
+            {
+                // No string in the list. Write 0 and return
+                ptr = Marshal.AllocHGlobal(sizeof(int));
+                Marshal.WriteInt32(ptr, 0);
+                return toRelease;
+            }
+
+            // Get the total size of unmanaged memory that is needed (length + elements)
+            int size = sizeof(int) + (IntPtr.Size * sequence.Count);
+
+            // Allocate unmanaged space.
+            ptr = Marshal.AllocHGlobal(size);
+
+            // Write the "Length" field first
+            Marshal.WriteInt32(ptr, sequence.Count);
+
+            // Write the pointers to the string data
+            for (int i = 0; i < sequence.Count; i++)
+            {
+                // Create a pointer to the string in unmanaged memory
+                IntPtr sPtr;
+                if (isUnicode)
+                {
+                    sPtr = Marshal.StringToHGlobalUni(sequence[i]);
+                }
+                else
+                {
+                    sPtr = Marshal.StringToHGlobalAnsi(sequence[i]);
+                }
+
+                // Add to pointer to the list of pointers to release
+                toRelease.Add(sPtr);
+
+                // Write the pointer location in ptr
+                Marshal.StructureToPtr(sPtr, ptr + sizeof(int) + (i * IntPtr.Size), false);
+            }
+
+            return toRelease;
+        }
+
         public static void ExecuteAnyCpu(Action x86Code, Action x64Code)
         {
             if (Environment.Is64BitProcess)
