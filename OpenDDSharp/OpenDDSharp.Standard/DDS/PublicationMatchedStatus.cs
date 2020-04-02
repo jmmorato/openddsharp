f3fd96d0860d0e14f2a2bd3a4df4b3d574999230
Jose Morato <jmmorato@enxarxats.com>
2020-04-02 17:11:07 +0200

    [ignore] Added documentation comments to public methods

--- OpenDDSharp/OpenDDSharp.Standard/DDS/PublicationMatchedStatus.cs
+++ OpenDDSharp/OpenDDSharp.Standard/DDS/PublicationMatchedStatus.cs
@@ -58,6 +58,11 @@ namespace OpenDDSharp.DDS
         #endregion
 
         #region IEquatable<PublicationMatchedStatus> Members
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns><see langword="true" /> if the current object is equal to the other parameter; otherwise, <see langword="false" />.</returns>
         public bool Equals(PublicationMatchedStatus other)
         {
             return TotalCount == other.TotalCount &&
@@ -67,11 +72,20 @@ namespace OpenDDSharp.DDS
                    LastSubscriptionHandle == other.LastSubscriptionHandle;
         }
 
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" />.</returns>
         public override bool Equals(object obj)
         {
             return (obj is PublicationMatchedStatus other) && Equals(other);
         }
 
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
         public override int GetHashCode()
         {
             var hashCode = 543595584;
@@ -85,11 +99,23 @@ namespace OpenDDSharp.DDS
         #endregion
 
         #region Operators
+        /// <summary>
+        /// Equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="true" /> if the left object is equal to the right object; otherwise, <see langword="false" />.</returns>
         public static bool operator ==(PublicationMatchedStatus left, PublicationMatchedStatus right)
         {
             return left.Equals(right);
         }
 
+        /// <summary>
+        /// Not equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="false" /> if the left object is equal to the right object; otherwise, <see langword="true" />.</returns>
         public static bool operator !=(PublicationMatchedStatus left, PublicationMatchedStatus right)
         {
             return !(left == right);
