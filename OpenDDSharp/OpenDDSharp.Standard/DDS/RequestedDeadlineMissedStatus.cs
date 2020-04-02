85f237b75e5485e00cd81ba65ff2a7ccd00997ce
Jose Morato <jmmorato@enxarxats.com>
2020-04-02 17:13:21 +0200

    [feature] Implemented RequestedDeadlineMissedStatus structure

--- OpenDDSharp/OpenDDSharp.Standard/DDS/RequestedDeadlineMissedStatus.cs
+++ OpenDDSharp/OpenDDSharp.Standard/DDS/RequestedDeadlineMissedStatus.cs
@@ -17,12 +17,93 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
 **********************************************************************/
+using System;
+using System.Runtime.InteropServices;
+
 namespace OpenDDSharp.DDS
 {
     /// <summary>
     /// The RequestedDeadlineMissed status indicates that the deadline requested via the <see cref="DeadlineQosPolicy" /> was not respected for a specific instance.
     /// </summary>
-    public struct RequestedDeadlineMissedStatus
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RequestedDeadlineMissedStatus : IEquatable<RequestedDeadlineMissedStatus>
     {
+        #region Properties
+        /// <summary>
+        /// Gets the cumulative count of missed requested deadlines that have been reported.
+        /// </summary>
+        public int TotalCount { get; }
+
+        /// <summary>
+        /// Gets the incremental count of missed requested deadlines since the last time this status was accessed.
+        /// </summary>
+        public int TotalCountChange { get; }
+
+        /// <summary>
+        /// Gets the instance handle of the last missed deadline.
+        /// </summary>
+        public InstanceHandle LastInstanceHandle { get; }
+        #endregion
+
+        #region IEquatable<RequestedDeadlineMissedStatus> Members
+        /// <summary>
+        /// Indicates whether the current object is equal to another object of the same type.
+        /// </summary>
+        /// <param name="other">An object to compare with this object.</param>
+        /// <returns><see langword="true" /> if the current object is equal to the other parameter; otherwise, <see langword="false" />.</returns>
+        public bool Equals(RequestedDeadlineMissedStatus other)
+        {
+            return TotalCount == other.TotalCount &&
+                   TotalCountChange == other.TotalCountChange &&
+                   LastInstanceHandle == other.LastInstanceHandle;
+        }
+
+        /// <summary>
+        /// Determines whether the specified object is equal to the current object.
+        /// </summary>
+        /// <param name="obj">The object to compare with the current object.</param>
+        /// <returns><see langword="true" /> if the specified object is equal to the current object; otherwise, <see langword="false" />.</returns>
+        public override bool Equals(object obj)
+        {
+            return (obj is RequestedDeadlineMissedStatus other) && Equals(other);
+        }
+
+        /// <summary>
+        /// Serves as the default hash function.
+        /// </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            var hashCode = 543595584;
+            hashCode = (hashCode * -1521134295) + TotalCount.GetHashCode();
+            hashCode = (hashCode * -1521134295) + TotalCountChange.GetHashCode();
+            hashCode = (hashCode * -1521134295) + LastInstanceHandle.GetHashCode();
+            return hashCode;
+        }
+        #endregion
+
+        #region Operators
+        /// <summary>
+        /// Equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="true" /> if the left object is equal to the right object; otherwise, <see langword="false" />.</returns>
+        public static bool operator ==(RequestedDeadlineMissedStatus left, RequestedDeadlineMissedStatus right)
+        {
+            return left.Equals(right);
+        }
+
+        /// <summary>
+        /// Not equals comparison operator.
+        /// </summary>
+        /// <param name="left">The left value for the comparison.</param>
+        /// <param name="right">The right value for the comparison.</param>
+        /// <returns><see langword="false" /> if the left object is equal to the right object; otherwise, <see langword="true" />.</returns>
+        public static bool operator !=(RequestedDeadlineMissedStatus left, RequestedDeadlineMissedStatus right)
+        {
+            return !(left == right);
+        }
+        #endregion
     }
 }
