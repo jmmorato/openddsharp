1558e4ade10dc6fbf51629be1ad31a74dadb95d9
Tim Pollock <pollockt@objectcomputing.com>
2020-09-01 07:53:42 -0500

    Changed NuGet version for OpenDDS v3.14

--- OpenDDSharp/OpenDDSharp/AssemblyInfo.h
+++ OpenDDSharp/OpenDDSharp/AssemblyInfo.h
@@ -29,5 +29,5 @@ along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
 #define Assembly_Copyright L"Copyright © Jose Morato 2018. All rights reserved"
 #define Assembly_Description L".NET wrapper for OpenDDS"
 #define File_Version 0,4,1,0
-#define File_Version_Str "0.4.1.0"
-#define Assembly_Version L"0.4.1.0"
+#define File_Version_Str "0.5.0.0"
+#define Assembly_Version L"0.5.0.0"
