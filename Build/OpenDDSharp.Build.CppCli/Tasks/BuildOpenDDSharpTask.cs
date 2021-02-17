8e625dded2df75fedd852e8d1e443b4e2e4bf5fd
Jose Morato <jmmorato@enxarxats.com>
2021-02-17 19:53:02 +0100

    MaxCpuCount=1 when building OpenDDSharp

--- Build/OpenDDSharp.Build.CppCli/Tasks/BuildOpenDDSharpTask.cs
+++ Build/OpenDDSharp.Build.CppCli/Tasks/BuildOpenDDSharpTask.cs
@@ -1,5 +1,4 @@
-﻿using Cake.Common;
-using Cake.Common.Tools.MSBuild;
+﻿using Cake.Common.Tools.MSBuild;
 using Cake.Frosting;
 using System.IO;
 
@@ -16,6 +15,7 @@ namespace OpenDDSharp.Build.CppCli.Tasks
                 Configuration = context.BuildConfiguration,
                 PlatformTarget = context.BuildPlatform,
                 Targets = { "Restore", "Build" },
+                MaxCpuCount = 1,
                 WorkingDirectory = Path.GetFullPath(BuildContext.OPENDDSHARP_SOLUTION_FOLDER),
                 EnvironmentVariables =
                 {
@@ -23,7 +23,6 @@ namespace OpenDDSharp.Build.CppCli.Tasks
                     { nameof(BuildContext.ACE_ROOT), Path.GetFullPath(BuildContext.ACE_ROOT).TrimEnd('\\') },
                     { nameof(BuildContext.TAO_ROOT), Path.GetFullPath(BuildContext.TAO_ROOT).TrimEnd('\\') },
                     { nameof(BuildContext.MPC_ROOT), Path.GetFullPath(BuildContext.MPC_ROOT).TrimEnd('\\') },
-                    //{ "Path", context.EnvironmentVariable("Path") + BuildContext.DDS_PATH_ADDITION },
                 },
             });
         }
