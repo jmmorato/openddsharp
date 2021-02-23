fd4caf1498d332da54bedd1584584ff8edcfa105
Jose Morato <jmmorato@enxarxats.com>
2021-02-23 14:34:25 +0100

    EnableCodeCoverage and try to resolve toolpath

--- Build/OpenDDSharp.Build.CppCli/Tasks/TestTask.cs
+++ Build/OpenDDSharp.Build.CppCli/Tasks/TestTask.cs
@@ -16,7 +16,9 @@ namespace OpenDDSharp.Build.CppCli.Tasks
             var file = "OpenDDSharp.UnitTest.dll";
             var platform = context.BuildPlatform == PlatformTarget.x64 ? VSTestPlatform.x64 : VSTestPlatform.x86;
             context.VSTest(path + file, new VSTestSettings
-            {                
+            {
+                ToolPath = context.Tools.Resolve("vstest.console.exe"),
+                EnableCodeCoverage = true,
                 PlatformArchitecture = platform,
                 WorkingDirectory = Path.GetFullPath(path),
                 EnvironmentVariables =
@@ -25,7 +27,6 @@ namespace OpenDDSharp.Build.CppCli.Tasks
                     { nameof(BuildContext.ACE_ROOT), Path.GetFullPath(BuildContext.ACE_ROOT).TrimEnd('\\') },
                     { nameof(BuildContext.TAO_ROOT), Path.GetFullPath(BuildContext.TAO_ROOT).TrimEnd('\\') },
                     { nameof(BuildContext.MPC_ROOT), Path.GetFullPath(BuildContext.MPC_ROOT).TrimEnd('\\') },
-                    //{ "Path", context.EnvironmentVariable("Path") + BuildContext.DDS_PATH_ADDITION },
                 },
                 SettingsFile = Path.Combine(Path.GetFullPath(BuildContext.OPENDDSHARP_SOLUTION_FOLDER), "CodeCoverage.runsettings"),
             });
