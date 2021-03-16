42412e176d2db4707a5ca466fe1ba3166f0313f3
Jose Morato <jmmorato@enxarxats.com>
2021-03-16 18:21:43 +0100

    Removed Clean tasks

--- Build/OpenDDSharp.Build.CppCli/Tasks/DefaultTask.cs
+++ Build/OpenDDSharp.Build.CppCli/Tasks/DefaultTask.cs
@@ -3,7 +3,6 @@
 namespace OpenDDSharp.Build.CppCli.Tasks
 {
     [TaskName("Default")]
-    [IsDependentOn(typeof(CleanTask))]
     [IsDependentOn(typeof(BuildTask))]
     [IsDependentOn(typeof(TestTask))]
     public class DefaultTask : FrostingTask
