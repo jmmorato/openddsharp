2c34b2ac450e80c904d245491d508ef823bc77e7
jmorato <jmmorato@enxarxats.com>
2021-04-24 18:19:29 +0200

    Updated license comment (#44)

--- Build/OpenDDSharp.Build.CppCli/Tasks/BuildThirdPartyTask.cs
+++ Build/OpenDDSharp.Build.CppCli/Tasks/BuildThirdPartyTask.cs
@@ -1,4 +1,23 @@
-﻿using Cake.Common;
+﻿/*********************************************************************
+This file is part of OpenDDSharp.
+
+OpenDDSharp is a .NET wrapper for OpenDDS.
+Copyright (C) 2018 - 2021 Jose Morato
+
+OpenDDSharp is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+OpenDDSharp is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with OpenDDSharp. If not, see <http://www.gnu.org/licenses/>.
+**********************************************************************/
+using Cake.Common;
 using Cake.Common.IO;
 using Cake.Common.Tools.MSBuild;
 using Cake.Frosting;
