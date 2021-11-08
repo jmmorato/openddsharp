2ecc086ff416ea6ea9d44aa377723989cdf17ee6
Jose Morato <jmmorato@enxarxats.com>
2021-11-08 16:50:08 +0100

    [feature] Added template to create console applications

---
+++ Sources/OpenDDSharp.Templates/templates/ConsoleProject/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using OpenDDSharp;
+using OpenDDSharp.DDS;
+using OpenDDSharp.OpenDDS.DCPS;
+
+namespace OpenDDSharp.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Ace.Init();
+
+            DomainParticipantFactory dpf = ParticipantService.Instance.GetDomainParticipantFactory("-DCPSConfigFile", "rtps.ini");
+            DomainParticipant participant = dpf.CreateParticipant(42);
+            if (participant == null)
+            {
+                throw new Exception("Could not create the participant");
+            }
+
+            // Include your program here
+
+            participant.DeleteContainedEntities();
+            dpf.DeleteParticipant(participant);
+            ParticipantService.Instance.Shutdown();
+
+            Ace.Fini();
+        }
+    }
+}
