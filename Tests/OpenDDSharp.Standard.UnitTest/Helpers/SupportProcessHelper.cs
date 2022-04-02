dff414b8d767bd8b629cdbf2322ecbfee30efc80
Jose Morato <jmmorato@enxarxats.com>
2022-04-02 20:27:59 +0200

    Use INFOREPO output parameter

--- Tests/OpenDDSharp.Standard.UnitTest/Helpers/SupportProcessHelper.cs
+++ Tests/OpenDDSharp.Standard.UnitTest/Helpers/SupportProcessHelper.cs
@@ -149,7 +149,7 @@ namespace OpenDDSharp.Standard.UnitTest.Helpers
                 throw new FileNotFoundException($"The support process executable could not be located at {infoRepoPath}.");
             }
 
-            return SpawnProcess(infoRepoPath, string.Empty);
+            return SpawnProcess(infoRepoPath, "-o file://repo.ior");
         }
 
         private Process SpawnProcess(string path, string arguments)
