3ea73a3c3fa0514ae69ef330df3e76a5a250663c
Jose Morato <jmmorato@enxarxats.com>
2022-04-02 20:37:05 +0200

    Fixed syntax error

--- Tests/OpenDDSharp.Standard.UnitTest/Helpers/SupportProcessHelper.cs
+++ Tests/OpenDDSharp.Standard.UnitTest/Helpers/SupportProcessHelper.cs
@@ -149,7 +149,7 @@ namespace OpenDDSharp.Standard.UnitTest.Helpers
                 throw new FileNotFoundException($"The support process executable could not be located at {infoRepoPath}.");
             }
 
-            return SpawnProcess(infoRepoPath, "-o file://repo.ior");
+            return SpawnProcess(infoRepoPath, @"-o repo.ior");
         }
 
         private Process SpawnProcess(string path, string arguments)
