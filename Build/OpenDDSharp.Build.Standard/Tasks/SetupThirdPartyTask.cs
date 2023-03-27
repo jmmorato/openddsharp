3d956a32de6ee4615b9d70a79cd8dabba4c7a2a8
jose_morato <jmmorato@enxarxats.com>
2023-03-27 18:21:35 +0200

    Added prefix

--- Build/OpenDDSharp.Build.Standard/Tasks/SetupThirdPartyTask.cs
+++ Build/OpenDDSharp.Build.Standard/Tasks/SetupThirdPartyTask.cs
@@ -109,7 +109,7 @@ namespace OpenDDSharp.Build.Standard.Tasks
             if (BuildContext.IsLinux || BuildContext.IsOSX)
             {
                 var configurePath = System.IO.Path.Combine(_clonePath.FullPath, "configure");
-                var arguments = " -v --ace-github-latest --no-test --no-debug --optimize --install-origin-relative";
+                var arguments = " -v --ace-github-latest --no-test --no-debug --optimize --install-origin-relative --prefix=/usr/lib";
                 if (BuildContext.IsOSX)
                 {
                     arguments += " --std=c++11";
