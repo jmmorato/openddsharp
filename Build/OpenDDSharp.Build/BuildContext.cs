6e9085fc16f301b6e7cc854bef1279d6b286398b
jmmorato <jmmorato@enxarxats.com>
2023-08-01 20:28:08 +0200

    [feature] Update to OpenDDS v3.25 (#188)

--- Build/OpenDDSharp.Build/BuildContext.cs
+++ Build/OpenDDSharp.Build/BuildContext.cs
@@ -32,7 +32,7 @@ namespace OpenDDSharp.Build
     public class BuildContext : FrostingContext
     {
         #region Constants
-        internal const string DEFAULT_OPENDDS_VERSION = "3.24.100";
+        internal const string DEFAULT_OPENDDS_VERSION = "3.25";
 
         private const string DEFAULT_PERL_FOLDER = "C:/Strawberry/perl/bin";
 
