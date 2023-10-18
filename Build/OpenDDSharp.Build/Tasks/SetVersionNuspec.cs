fd9586374064ef2b81a1edb13de344af054a315a
jose_morato <jmmorato@enxarxats.com>
2023-10-18 17:51:36 +0200

    Update version when packing

--- Build/OpenDDSharp.Build/Tasks/SetVersionNuspec.cs
+++ Build/OpenDDSharp.Build/Tasks/SetVersionNuspec.cs
@@ -36,7 +36,11 @@ namespace OpenDDSharp.Build.Tasks
         {
             context.Log.Information("Set version in NuSpec...");
 
-            var version = $"{context.MajorVersion}.{context.MinorVersion}.{context.PatchVersion}.{context.BuildNumber}{context.PreReleaseTag}";
+            var version = $"{context.MajorVersion}.{context.MinorVersion}.{context.PatchVersion}";
+            if (!string.IsNullOrWhiteSpace(context.PreReleaseTag))
+            {
+                version += $"-{context.PreReleaseTag}{context.BuildNumber}";
+            }
 
             var path = context.MakeAbsolute(context.Directory(BuildContext.OPENDDSHARP_SOLUTION_FOLDER));
 
