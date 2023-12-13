6d8b122707bacc17b8b8a6fe9c437c4c0117eabf
jose_morato <jmmorato@enxarxats.com>
2023-12-13 10:02:38 +0100

    [feature] CDR primitive sequences serialization

--- Tests/BenchmarkPerformance/PerformanceTests/OpenDDSharpLatencyTest.cs
+++ Tests/BenchmarkPerformance/PerformanceTests/OpenDDSharpLatencyTest.cs
@@ -1,7 +1,7 @@
 using System.Globalization;
 using OpenDDSharp.DDS;
 using OpenDDSharp.OpenDDS.DCPS;
-using JsonWrapper;
+using CdrWrapper;
 
 namespace OpenDDSharp.BenchmarkPerformance.PerformanceTests;
 
@@ -78,18 +78,32 @@ internal sealed class OpenDDSharpLatencyTest : IDisposable
 
     private void InitializeDDSEntities()
     {
-        _dpf = ParticipantService.Instance.GetDomainParticipantFactory();
+        _dpf = ParticipantService.Instance.GetDomainParticipantFactory("-DCPSInfoRepo", "file://repo.ior");
 
-        _participant = _dpf.CreateParticipant(DOMAIN_ID);
         var guid = Guid.NewGuid().ToString("N", CultureInfo.InvariantCulture);
         var configName = "openddsharp_rtps_interop_" + guid;
         var instName = "internal_openddsharp_rtps_transport_" + guid;
 
         var config = TransportRegistry.Instance.CreateConfig(configName);
         var inst = TransportRegistry.Instance.CreateInst(instName, "rtps_udp");
-        var rui = new RtpsUdpInst(inst);
+        var rui = new RtpsUdpInst(inst)
+        {
+            UseMulticast = false,
+            LocalAddress = "localhost",
+            NakResponseDelay = new TimeValue
+            {
+                Seconds = 0,
+                MicroSeconds = 50_000,
+            },
+            HeartbeatPeriod = new TimeValue
+            {
+                Seconds = 0,
+                MicroSeconds = 50_000,
+            },
+        };
         config.Insert(rui);
 
+        _participant = _dpf.CreateParticipant(DOMAIN_ID);
         TransportRegistry.Instance.BindConfig(configName, _participant);
 
         var typeSupport = new KeyedOctetsTypeSupport();
@@ -103,16 +117,28 @@ internal sealed class OpenDDSharpLatencyTest : IDisposable
         var dwQos = new DataWriterQos
         {
             Reliability = { Kind = ReliabilityQosPolicyKind.ReliableReliabilityQos },
+            History =
+            {
+                Kind = HistoryQosPolicyKind.KeepLastHistoryQos,
+                Depth = 1,
+            },
         };
         var dw = _publisher.CreateDataWriter(_topic, dwQos);
+        TransportRegistry.Instance.BindConfig(configName, dw);
         _dataWriter = new KeyedOctetsDataWriter(dw);
 
         _subscriber = _participant.CreateSubscriber();
         var drQos = new DataReaderQos
         {
             Reliability = { Kind = ReliabilityQosPolicyKind.ReliableReliabilityQos },
+            History =
+            {
+                Kind = HistoryQosPolicyKind.KeepLastHistoryQos,
+                Depth = 1,
+            },
         };
         var dr =  _subscriber.CreateDataReader(_topic, drQos);
+        TransportRegistry.Instance.BindConfig(configName, dr);
         _dataReader = new KeyedOctetsDataReader(dr);
 
         _waitSet = new WaitSet();
@@ -128,15 +154,20 @@ internal sealed class OpenDDSharpLatencyTest : IDisposable
 
     private void ReaderThreadProc()
     {
+        var duration = new Duration
+        {
+            Seconds = 1,
+            NanoSeconds = 0,
+        };
         while (true)
         {
             var conditions = new List<Condition>();
-            _waitSet.Wait(conditions);
+            _waitSet.Wait(conditions, duration);
 
-            var samples = new List<KeyedOctets>();
-            var sampleInfos = new List<SampleInfo>();
-            _dataReader.Take(samples, sampleInfos);
-            _count += 1;//samples.Count;
+            var sample = new KeyedOctets();
+            var sampleInfo = new SampleInfo();
+            _dataReader.TakeNextSample(sample, sampleInfo);
+            _count += 1;
 
             _evt.Set();
 
@@ -144,24 +175,6 @@ internal sealed class OpenDDSharpLatencyTest : IDisposable
             {
                 return;
             }
-
-            // foreach (var cond in  conditions)
-            // {
-            //     if (cond == _statusCondition && cond.TriggerValue)
-            //     {
-            //         var samples = new List<KeyedOctets>();
-            //         var sampleInfos = new List<SampleInfo>();
-            //         _dataReader.Take(samples, sampleInfos);
-            //         _count += 1;//samples.Count;
-            //
-            //         _evt.Set();
-            //
-            //         if (_count == _totalSamples * _totalInstances)
-            //         {
-            //             return;
-            //         }
-            //     }
-            // }
         }
     }
 
