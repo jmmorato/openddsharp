9e4914cab411c676d31d1c363f45be124ff95a1a
jose_morato <jmmorato@enxarxats.com>
2025-05-15 18:36:17 +0200

    Removed Debug parameters

--- Examples/ConsoleDemo/Program.cs
+++ Examples/ConsoleDemo/Program.cs
@@ -39,8 +39,8 @@ namespace ConsoleDemo
             Ace.Init();
 
             Console.WriteLine("Get domain participant factory...");
-            var dpf = ParticipantService.Instance.GetDomainParticipantFactory("-DCPSConfigFile", "rtps.ini",
-                "-DCPSDebugLevel", "10", "-ORBLogFile", "LogFile.log", "-ORBDebugLevel", "10");
+            // Debug parameters: "-DCPSDebugLevel", "10", "-ORBLogFile", "LogFile.log", "-ORBDebugLevel", "10"
+            var dpf = ParticipantService.Instance.GetDomainParticipantFactory("-DCPSConfigFile", "rtps.ini");
             if (dpf == null)
             {
                 Console.Error.WriteLine("Domain participant factory could NOT be created.");
